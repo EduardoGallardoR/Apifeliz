940ae5876b542c93888f9090d2f52d0f4d2d3add
EduardoGallardoR <egallardo821@gmail.com>
2023-12-04 17:23:40 -0600

    renderbd

--- APIEDU/settings.py
+++ APIEDU/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-cv-!hbuz6!5y$sp5u%djp0=&19zr4%fzdqcbmt==k6d)-4n7^8'
 
-# SECURITY WARNING: don't run with debug turned on in production!
+#  SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
 ALLOWED_HOSTS = ['*']
